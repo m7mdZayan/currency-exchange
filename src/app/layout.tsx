48f85a9b3332094cfe81501dc1c38aef4ed262a9
Mohamed Zayan <mzayan@lucidya.com>
2025-03-08 23:12:33 +0200

    add open graph to the app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,14 @@ export const metadata: Metadata = {
   title: "Currency exchange App",
   description:
     "this app has some cool features like currency conversion, filtering and sorting them.",
+  openGraph: {
+    title: "Currency Exchange App",
+    description:
+      "Easily convert currencies and track live exchange rates with our intuitive app.",
+    url: "https://currency-exchange-mzayan.vercel.app/",
+    siteName: "Currency Exchange",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
